a6785f04a5b2559c4ac68609330399dacfc1fd29
s-cumpalovic <stefancumpalovic98@gmail.com>
2022-06-06 18:40:22 +0200

    Added seperate .php files, and included files where necessary, will be updated later

---
+++ create-post.php
@@ -0,0 +1,4 @@
+<?php
+include_once "./partial-files/header.php";
+include_once "./partial-files/footer.php";
+include_once "./partial-files/sidebar.php";
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+include_once "./partial-files/header.php";
+include_once "./partial-files/footer.php";
+include_once "./partial-files/sidebar.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+        integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+         <!-- Custom styles for this template -->
+        <link href="styles/blog.css" rel="stylesheet">
+
+    <title>Vivify Blog</title>
+</head>
+<body>
+
+</body>
+</html>
